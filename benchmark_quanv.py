45e47c334a9e4539645c8bac701aed1746a342be
Vu Tuan Hai <haimeohung@gmail.com>
2025-03-11 16:21:58 +0900

    Rerun for quanv1 3 qubit case

--- benchmark_quanv.py
+++ benchmark_quanv.py
@@ -6,12 +6,12 @@ import numpy as np
 
 # Benchmark from quanvolutional1 to quanvolutional19
 
-for i in range(1, 20):
+for i in [1,1,1,1]:
     timess = []
-    for num_qubits in range(3, 18):
+    for num_qubits in range(3, 4):
         function_name = f'quanvolutional{i}'
         times = []
-        for t in range(0, 1):
+        for t in range(0, 100):
             start = time.time()
             qc = qiskit.QuantumCircuit(num_qubits) 
             qc = getattr(ansatz, function_name)(qc)
@@ -20,4 +20,5 @@ for i in range(1, 20):
             end = time.time()
             times.append(end-start)
         timess.append(np.mean(times))
-    np.savetxt(f'result/quanv/quanv{i}.txt', timess)
+        print(np.mean(times))
+    # np.savetxt(f'result/quanv/quanv{i}.txt', timess)
