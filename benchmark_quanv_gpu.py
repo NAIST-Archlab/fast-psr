808fe44206d89715be0ba8b5e2b8f43683996bfb
Vu Tuan Hai <haimeohung@gmail.com>
2025-05-26 13:18:17 +0900

    Fix import

--- benchmark_quanv_gpu.py
+++ benchmark_quanv_gpu.py
@@ -1,23 +1,21 @@
 import qiskit
-import benchmark.ansatz as ansatz
 import time
 import numpy as np
 from qiskit_aer import Aer
-
-# Benchmark from quanvolutional1 to quanvolutional19
-
-for i in range(1, 2):
+import ansatz
+for i in range(1, 3):
     timess = []
-    for num_qubits in range(3, 5):
+    for num_qubits in range(3, 6):
         function_name = f'quanvolutional{i}'
         times = []
         for t in range(0, 10):
             start = time.time()
             qc = qiskit.QuantumCircuit(num_qubits) 
+            
             qc = getattr(ansatz, function_name)(qc)
             qc.save_statevector()
             simulator = Aer.get_backend('aer_simulator_statevector', device = 'GPU')
-            qc = qiskit.transpile(qc, backend = simulator, basis_gates=['h', 's', 'cx', 'rx', 'ry', 'rz'])
+            qc = qiskit.transpile(qc, backend = simulator)
             result = simulator.run(qc).result()
             statevector = result.get_statevector(qc)
             end = time.time()
