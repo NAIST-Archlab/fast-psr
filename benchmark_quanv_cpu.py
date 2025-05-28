9c076fd3989887cefd6e5f6d6bbaaa382bdf5425
Vu Tuan Hai <haimeohung@gmail.com>
2025-05-28 11:12:15 +0900

    Update benchmark

--- benchmark_quanv_cpu.py
+++ benchmark_quanv_cpu.py
@@ -5,18 +5,15 @@ from qiskit_aer import Aer
 import ansatz
 import pandas as pd
 
-def quanv(num_qubits, function_name):
+def quanv_cpu(num_qubits, function_name):
     qc = qiskit.QuantumCircuit(num_qubits) 
     qc = getattr(ansatz, function_name)(qc)
-    qc.save_statevector()
-    simulator = Aer.get_backend('aer_simulator_statevector', device = 'GPU')
-    qc = qiskit.transpile(qc, backend = simulator)
-    result = simulator.run(qc).result()
-    statevector = result.get_statevector(qc)
+    qc = qiskit.transpile(qc, basis_gates=['h', 's', 'cx', 'rx', 'ry', 'rz'], optimization_level=3)
+    state = qiskit.quantum_info.Statevector.from_instruction(qc)
     return
 
 for i in range(1, 10):
-    quanv(10, 'quanvolutional1')  # Warm up the GPU
+    quanv_cpu(10, 'quanvolutional1')  # Warm up the GPU
 for num_qubits in range(3, 30):
     for i in range(1, 20):
         
@@ -24,12 +21,12 @@ for num_qubits in range(3, 30):
         times = []
         for t in range(0, 10):
             start = time.time()
-            quanv(num_qubits, function_name)
+            quanv_cpu(num_qubits, function_name)
             end = time.time()
             times.append(end-start)
         record = {'num_qubits': num_qubits, 'quanvolutional': i, 'time': np.mean(times)}
         if 'results_df' not in locals():
             results_df = pd.DataFrame(columns=record.keys())
         results_df = pd.concat([results_df, pd.DataFrame([record])], ignore_index=True)
-        results_df.to_csv(f'result/quanv_gpu30.csv', index=False)
+        results_df.to_csv(f'result/quanv_cpu30.csv', index=False)
         print(f'num_qubits: {num_qubits}, quanvolutional{i}, time: ', np.mean(times))
